1ab27be80f5694f5e2bcfeff1957a530456859ce
DanielMendieta <110202705+DanielMendieta@users.noreply.github.com>
2023-07-10 23:49:12 -0300

    comercio.py

--- comercio.py
+++ comercio.py
@@ -58,7 +58,7 @@ def redshiftDB():
     urll="data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws"
     data_base="data-engineer-database"
     user="mendietadaniel1994_coderhouse"
-    pwd= 'o4T3Iuw898'
+    pwd= 'xxxxx'
         
     try:
         conn = psycopg2.connect(
